fa97554002be29b982da8e1659ffad8b3dfc101a
rayffer0001 <rayffer@hotmail.es>
2023-04-12 21:11:10 -0500

    Comienzo de la maquetacion con bootstrap

---
+++ src/styles.js
@@ -0,0 +1,7 @@
+//funciona como un tree checkeable 
+//importar la libreria boostrap
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//cargar libreria para alertas y iconos
+import '@fortawesome/fontawesome-free/css/all.min.css'//que cargue todo lo que tenga en ese archivo
